5a770b6817344034209db9384b172503874c5acc
Thomas Zhang <thomaszhang718@gmail.com>
2016-12-09 23:42:58 -0500

    Change db to mongolab for heroku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/lemcy' // 'mongodb://heroku_w06pvxcw:mn2ditm597pb8ahc80l9snkmk2@ds153657.mlab.com:53657/heroku_w06pvxcw'
+    'url' : 'mongodb://heroku_w06pvxcw:mn2ditm597pb8ahc80l9snkmk2@ds153657.mlab.com:53657/heroku_w06pvxcw'
+    //'url' : 'mongodb://localhost/lemcy'
 
 };
